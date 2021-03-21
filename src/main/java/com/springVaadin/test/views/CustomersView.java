7b5b16a00a3ba430a072b765facf8217e8d0da19
Vasilis Tsimpragakis <vtsimpra@gmail.com>
2021-03-21 21:20:13 +0200

    Created CustomerEditor. Binded editor and View , added CRUD functionalities.

--- src/main/java/com/springVaadin/test/views/CustomersView.java
+++ src/main/java/com/springVaadin/test/views/CustomersView.java
@@ -5,6 +5,8 @@ import com.springVaadin.test.services.CustomerService;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
@@ -17,17 +19,50 @@ public class CustomersView extends VerticalLayout {
 
     private CustomerService customerService;
     private final Grid<Customer> grid;
+    private final CustomerEditor editor;
 
-    public CustomersView(CustomerService customerService) {
+    public CustomersView(CustomerService customerService, CustomerEditor editor) {
         add(new Button("Back to Home",e-> UI.getCurrent().navigate(MainView.class)));
         this.customerService=customerService;
         this.grid = new Grid<>(Customer.class);
+        this.editor=editor;
+
+        // build layout
         TextField textFilter = new TextField();
         textFilter.setPlaceholder("Filter by last name");
+
+        Button addNewBtn = new Button("New customer", VaadinIcon.PLUS.create());
+
+        HorizontalLayout actions = new HorizontalLayout(textFilter, addNewBtn);
+
+        grid.setHeight("300px");
+        grid.setColumns("id", "firstName", "lastName");
+        grid.getColumnByKey("id").setWidth("50px").setFlexGrow(0);
+
+        // Hook logic to components
+
+        // Replace listing with filtered content when user changes filter
         textFilter.setValueChangeMode(ValueChangeMode.EAGER);
         textFilter.addValueChangeListener(e -> grid.setItems(listCustomers(e.getValue())));
+
+        // Connect selected Customer to editor or hide if none is selected
+        grid.asSingleSelect().addValueChangeListener(e -> {
+            editor.editCustomer(e.getValue());
+        });
+
+        // Instantiate and edit new Customer the new button is clicked
+        addNewBtn.addClickListener(e -> editor.editCustomer(new Customer("", "")));
+
+        // Listen changes made by the editor, refresh data from backend
+        editor.setChangeHandler(() -> {
+            editor.setVisible(false);
+            grid.setItems(listCustomers(textFilter.getValue()));
+        });
+
+        //instantiate list
         grid.setItems(listCustomers(""));
-        add(textFilter,grid);
+
+        add(actions, grid, editor);
     }
 
     private List<Customer> listCustomers(String textFilter){
