414bceefda483dcf4c4715a7963babf3e8889bec
Vasilis Tsimpragakis <vtsimpra@gmail.com>
2021-03-21 16:44:44 +0200

    Enchance MainView with List of Customers.

--- src/main/java/com/springVaadin/test/views/MainView.java
+++ src/main/java/com/springVaadin/test/views/MainView.java
@@ -1,6 +1,9 @@
 package com.springVaadin.test.views;
 
+import com.springVaadin.test.domains.Customer;
+import com.springVaadin.test.services.CustomerService;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -8,7 +11,16 @@ import com.vaadin.flow.router.Route;
 @Route
 public class MainView extends VerticalLayout {
 
-    public MainView() {
+    private CustomerService customerService;
+    private final Grid<Customer> grid;
+
+    public MainView(CustomerService customerService) {
         add(new Button("Click me", e -> Notification.show("Hello, Spring+Vaadin user!")));
+
+        this.customerService=customerService;
+        this.grid = new Grid<>(Customer.class);
+        grid.setItems(customerService.getAllCustomers());
+        add(grid);
     }
+
 }
